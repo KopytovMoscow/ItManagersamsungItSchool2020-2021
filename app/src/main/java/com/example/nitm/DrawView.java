7d65c8d30a93ebe7cf97a5810f921b6ecd427830
Коля <Y98ojun1@yandex.ru>
2021-04-29 00:17:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nitm/DrawView.java
@@ -0,0 +1,69 @@
+package com.example.nitm;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+class DrawView extends View {
+    private int sx1;
+    private int sy1;
+    private int ex1;
+    private int ey1;
+    private int yyy = 0;
+    Paint paint;
+    Map<Button, ArrayList> dictionary = new HashMap<Button, ArrayList>();;
+
+    public DrawView(Context context, int sx, int sy, int ex, int ey) {
+        super(context);
+        ex1 = ex;
+        ey1 = ey;
+        sx1 = sx;
+        sy1 = sy;
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE); // установим белый цвет
+        paint.setStrokeWidth(5);
+//        paint.setStyle(Paint.Style.FILL); // заливаем
+        paint.setAntiAlias(true);
+    }
+
+    //    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint pai = new Paint();
+        pai.setColor(Color.WHITE); // установим белый цвет
+        pai.setStrokeWidth(5);
+        pai.setStyle(Paint.Style.FILL); // заливаем
+        pai.setAntiAlias(true);
+        if (dictionary != null){
+            for (Map.Entry<Button, ArrayList> entry : dictionary.entrySet()) {
+                ArrayList btns = (ArrayList) entry.getValue();
+                System.out.println(btns);
+                System.out.println(btns.get(0));
+                int sx = (int) (entry.getKey().getX() +
+                        (entry.getKey().getLayoutParams().width / 2));
+                int sy = (int) (entry.getKey().getY() +
+                        (entry.getKey().getLayoutParams().height / 2));
+                for (int i = 0; i < btns.size(); i++) {
+                    Button btn = (Button) btns.get(i);
+                    int ex = (int) (btn.getX() + (btn.getLayoutParams().width / 2));
+                    int ey = (int) (btn.getY() + (btn.getLayoutParams().height / 2));
+//                                                   relativeLayout.addView(new DrawView(thisContext, sx, sy, ex, ey));
+
+                    canvas.drawLine(sx, sy, ex, ey, pai);
+                }
+            }
+
+        }
+    }
+
+    public void invalidate(Map<Button, ArrayList> dictionary1) {
+        dictionary = dictionary1;
+        super.invalidate();
+    }
+}
---
+++ app/src/main/java/com/example/nitm/MainActivity.java
@@ -0,0 +1,218 @@
+package com.example.nitm;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private RelativeLayout relativeLayout;
+    private Canvas canvas;
+    private TextView textView;
+    EditText blockName;
+    ImageView imageView;
+    Button[] twoVertex = new Button[2];
+    Map<Button, ArrayList> dictionary = new HashMap<Button, ArrayList>();
+    Context thisContext;
+    private DrawView vertexView;
+
+    protected void transmission(){}
+
+
+    class AsyncRequest extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... arg) {
+            try {
+                System.out.println("ttttttttttttttttttttttttttttttttttttttttttt");
+                Socket s = new Socket("127.0.0.1", 9090);
+                System.out.println("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+                getStructure("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "as";
+        }
+    }
+
+    public static int getStructure(String link) throws IOException {
+        System.out.println("\t\t\t\t1");
+        Socket s = new Socket("127.0.0.1", 9090);
+        System.out.println("\t\t\t\t2");
+        InputStream in = s.getInputStream();
+        System.out.println("\t\t\t\t3");
+        OutputStream out = s.getOutputStream();
+        System.out.println("\t\t\t\t4");
+        System.out.println(out);
+        String str = link;
+        byte buf[] = str.getBytes();
+        out.write(buf);
+        int size;
+        byte buf_out[] = new byte[1024];
+        while ((size = in.read(buf_out)) != -1) {
+            System.out.print(new String(buf_out, 0, size));
+        }
+        s.close();
+        return 0;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        textView = (TextView) findViewById(R.id.textView);
+        blockName = (EditText) findViewById(R.id.editTextTextPersonName);
+        Button[] twoVertex = new Button[2]; // array that contain from 0 to 2 vertex to draw an edge between it
+        vertexView = new DrawView(this, 0, 0, 100, 200);
+        relativeLayout.addView(vertexView);
+//        String aw[] = new String[] {"1", "2", "3"};
+//        String bw[][] = new String[][] {{"1", "3"}, {"1", "2"}, {"2", "3"}};
+//        loadTheStructure(new String[]{"1", "2", "3"}, bw);
+        new AsyncRequest().execute("123", "/ajax", "foo=bar");
+
+    }
+
+    protected Button BlockInitialized(String text){
+        Button new_btn = new Button(this);
+        new_btn.setText(text);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        imageView = (ImageView) findViewById(R.id.aaa);
+        thisContext = this;
+
+//        relativeLayout.addView(aye);
+        new_btn.setText(text);
+        new_btn.setLayoutParams(
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT)
+        );
+        new_btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v){System.out.println("onLongClick"); return false; }
+        });
+
+        new_btn.setOnClickListener(new DoubleClickListener() {
+            @Override
+            public void onSingleClick(View v) {
+
+            }
+
+            @Override
+            public void onDoubleClick(View v){
+                RelativeLayout.LayoutParams linnear_lay = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT); // высота и
+                linnear_lay.leftMargin = (int) new_btn.getX();
+                linnear_lay.topMargin = (int) new_btn.getY();
+                if (twoVertex[0] == null){
+                    twoVertex[0] = (Button) v;
+                    v.setBackgroundColor(Color.YELLOW);
+
+                }
+                else{
+                    List ast = new ArrayList();
+                    if ( Arrays.asList(dictionary.keySet()).contains(twoVertex[0])){
+                        ast = (List) dictionary.get(twoVertex[0]);
+                    }
+                    ast.add((Button) v);
+                    dictionary.put(twoVertex[0], (ArrayList) ast);
+                    int sx = (int) (twoVertex[0].getX() +
+                            (twoVertex[0].getLayoutParams().width / 2));
+                    int sy = (int) (twoVertex[0].getY() +
+                            (twoVertex[0].getLayoutParams().height / 2));
+                    int ex = (int) (v.getX() + (v.getLayoutParams().width / 2));
+                    int ey = (int) (v.getY() + (v.getLayoutParams().height / 2));
+                    vertexView.invalidate(dictionary);
+                    twoVertex[0].setBackgroundColor(Color.GRAY);
+                    twoVertex[0] = null;
+                    twoVertex[1] = null;
+                }
+
+                new_btn.setLayoutParams(linnear_lay);
+            }
+        });
+
+        new_btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int a = (int) v.getX();
+                String b = Integer.toString(a);
+                int c = (int) v.getY();
+                String d = Integer.toString(c);
+                textView.setText(b + " " + d);
+                RelativeLayout.LayoutParams linnear_lay = new RelativeLayout.LayoutParams(
+                        new_btn.getWidth(),
+                        new_btn.getHeight()); // высота и
+                linnear_lay.leftMargin = (int) event.getRawX() - 200; // отступ
+                linnear_lay.topMargin = (int) event.getRawY() - 300; // отступ
+                new_btn.setLayoutParams(linnear_lay);
+                vertexView.invalidate(dictionary);
+                return false;
+            }
+        });
+        relativeLayout.addView(new_btn);
+        return new_btn;
+
+    }
+
+    public void newBlock(View view){
+        String name = String.valueOf(blockName.getText());
+        BlockInitialized(name);
+    }
+
+    protected void loadTheStructure(String[] v, String[][] e){
+        Map<String, Button> bb = new HashMap<String, Button>();
+        for (int i=0; i < v.length; i++){
+            System.out.println(v[i]);
+            Button a11 = BlockInitialized(v[i]);
+//            bb.put(, new Button(this));
+            bb.put(v[i], (Button) a11);}
+        for (int i=0; i < e.length; i++){
+            ArrayList l = new ArrayList();
+
+            l.add(bb.get(e[i][1]));
+            System.out.println(e[i][0] + " 888888888888888 " + e[i][1]);
+            dictionary.put((Button) bb.get(e[i][0]), (ArrayList) l);
+        }
+    }
+
+    protected void alternativeStructureBuilder(Map<String, List<String>> gitStructure){
+        System.out.println(gitStructure);
+    }
+
+    public void Load(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Важное сообщение!")
+                .setMessage("Закройте окно!")
+                .setCancelable(false)
+                .setNegativeButton("ОК, иду на балкон",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
